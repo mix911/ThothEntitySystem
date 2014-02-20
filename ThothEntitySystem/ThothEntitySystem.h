1ad6a7e6c6cfd8787a38130c183724348ddab960
mix911 <mihanale@gmail.com>
2014-02-21 02:03:16 +0400

    Initial Commit

---
+++ ThothEntitySystem/ThothEntitySystem.h
@@ -0,0 +1,13 @@
+//
+//  ThothEntitySystem.h
+//  ThothEntitySystem
+//
+//  Created by demo on 21.02.14.
+//  Copyright (c) 2014 demo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThothEntitySystem : NSObject
+
+@end
---
+++ ThothEntitySystem/ThothEntitySystem.m
@@ -0,0 +1,13 @@
+//
+//  ThothEntitySystem.m
+//  ThothEntitySystem
+//
+//  Created by demo on 21.02.14.
+//  Copyright (c) 2014 demo. All rights reserved.
+//
+
+#import "ThothEntitySystem.h"
+
+@implementation ThothEntitySystem
+
+@end
